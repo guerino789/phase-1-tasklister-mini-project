abe02ffbf135999d96b64f8ec7666d6445b3fe18
guerino antonini <guerantonini94@gmail.com>
2021-08-22 09:09:41 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTodo(e.target.new_task_description.value)
+    form.reset()
+  })
 });
+
+function createTodo(todo){
+  let p = document.createElement('p')
+  p.textContent = todo
+  document.getElementById('tasks').appendChild(p)
+}
